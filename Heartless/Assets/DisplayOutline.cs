b216c23a36333a60d4c60c7f097dd776d788fa4e
Cubond <giacomoromanello@gmail.com>
2020-11-21 17:00:11 +0100

    Mirror Collider for outline

---
+++ Heartless/Assets/DisplayOutline.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayOutline : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
