a80f5ce160bcd56b2f2244022147ce35f58f7693
jebbo <r.iervolino93@gmail.com>
2020-11-22 00:12:44 +0100

    camera

---
+++ Heartless/Assets/Script/MyCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCamera : MonoBehaviour
+{
+    public GameObject target;
+    public float cameraSmooth = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, -10), Time.deltaTime * cameraSmooth);
+    }
+}
