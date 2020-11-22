53f76054c5f31354bae52c894ddb8e4992f8877d
jebbo <r.iervolino93@gmail.com>
2020-11-22 01:05:50 +0100

    EnemyAndGenerators FSM++

---
+++ Heartless/Assets/Script/Generator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public bool broken = false;
+    float timer = 0;
+    
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            timer += Time.deltaTime;
+            if (timer > 1) broken = true;
+        }
+        if (collision.gameObject.tag == "Enemy")
+        {
+            timer += Time.deltaTime;
+            if (timer > 1) broken = false;
+        }
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        timer = 0;
+    }
+}
