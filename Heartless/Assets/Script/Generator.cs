aa512df6163874badc9a4e71b0ea8f6f3549e010
Cubond <giacomoromanello@gmail.com>
2020-11-22 14:29:13 +0100

    Rollback Generator.cs to working version

--- Heartless/Assets/Script/Generator.cs
+++ Heartless/Assets/Script/Generator.cs
@@ -1,26 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
-public class Generator : MonoBehaviour
+public class Generator : MonoBehaviour, I_Interactable
 {
     public bool broken = false;
     float timer = 0;
+
+    public GameObject linkedLight;
     
-    private void OnCollisionStay2D(Collision2D collision)
+    public void PlayerEnterRange()
     {
-        if (collision.gameObject.tag == "Player")
+        GlobalGameManager.instance.SelectedInteractableObj = gameObject;
+    }
+
+    public void PlayerExitRange()
+    {
+        GlobalGameManager.instance.SelectedInteractableObj = null;
+    }
+
+    public void Interact()
+    {
+        if (!broken)
         {
-            timer += Time.deltaTime;
-            if (timer > 1) broken = true;
+            broken = true;
+            GetComponent<Animator>().SetTrigger("break");
+            if (linkedLight != null)
+            {
+                linkedLight.GetComponentInChildren<Light2D>().enabled = false;
+            }
         }
+    }
+    
+    private void OnTriggerStay2D(Collider2D collision)
+    {
         if (collision.gameObject.tag == "Enemy")
         {
             timer += Time.deltaTime;
-            if (timer > 1) broken = false;
+            if (timer > 1)
+            {
+                broken = false;
+                timer = 0;
+                GetComponent<Animator>().SetTrigger("repair");
+                if (linkedLight != null)
+                {
+                    linkedLight.GetComponentInChildren<Light2D>().enabled = true;
+                }
+            }
         }
-
     }
+    
     private void OnCollisionExit2D(Collision2D collision)
     {
         timer = 0;
