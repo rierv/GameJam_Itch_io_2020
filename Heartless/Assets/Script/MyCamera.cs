abb9144cc06ea8c02fa10d3d69a2128e8759d0be
jebbo <r.iervolino93@gmail.com>
2020-11-25 15:22:56 +0100

    camera movement git add *!

--- Heartless/Assets/Script/MyCamera.cs
+++ Heartless/Assets/Script/MyCamera.cs
@@ -6,15 +6,19 @@ public class MyCamera : MonoBehaviour
 {
     public GameObject target;
     public float cameraSmooth = 1;
+    public float cameraMovementonDirection = 3;
+    Vector3 offSet;
     // Start is called before the first frame update
     void Start()
     {
-        
+        offSet = Camera.main.ScreenToViewportPoint(new Vector2(Camera.main.scaledPixelWidth, Camera.main.scaledPixelHeight) / 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, -10), Time.deltaTime * cameraSmooth);
+        Vector2 movement = Camera.main.ScreenToViewportPoint(Input.mousePosition)-offSet;
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, -10)+
+            new Vector3(movement.x, movement.y, -10)*cameraMovementonDirection, Time.deltaTime * cameraSmooth);
     }
 }
