273f32c04cfc7c2860aeb99052e63bdb06a6fb80
Cubond <giacomoromanello@gmail.com>
2020-11-27 15:53:22 +0100

    Scale e Bucozzo. Bucozzo not yet implemented

---
+++ Heartless/Assets/Bucozzo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucozzo : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (Input.GetKey(KeyCode.L))
+        {
+            BreakBucozzo();
+        }
+        */
+    }
+
+    public void BreakBucozzo()
+    {
+        Animator[] allAnim = GetComponentsInChildren<Animator>();
+        foreach (var anim in allAnim)
+        {
+            anim.SetTrigger("breakTile");
+        }
+    }
+}
